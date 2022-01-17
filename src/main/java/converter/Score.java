d6f7221a852515ce6490dbe50f798d5b0558785d
Kris Singh <krissingh2002@gmail.com>
2022-01-17 11:48:10 -0500

    made a comment to test pushing/pulling

--- src/main/java/converter/Score.java
+++ src/main/java/converter/Score.java
@@ -32,7 +32,7 @@ import utility.ValidationError;
 
 // This comment is a change made by Shahab Jami to test pushing and pulling to github
 
-// This comment is from Kris Singh to test pushing/pulling to github
+// This comment is from Kris Singh to test pushing and pulling to github
 
 public class Score extends ScoreComponent {
 
